1b92c1d46644b3b15792f33c7f0156c76fdd469f
LucasCodes01 <lucascodesweb@gmail.com>
2022-03-26 09:26:09 +1300

    Surrounded icons with divs + added caret animation

--- client/src/components/sidebar/SidebarExpand.tsx
+++ client/src/components/sidebar/SidebarExpand.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaCaretRight } from 'react-icons/fa';
+import { rootCertificates } from 'tls';
 
 const SidebarExpand = () => {
+  const [style, setStyle] = useState({});
+  const [expanded, setExpanded] = useState('');
+
+  const rotateCaret = (e:any) => {
+    console.log('Clicked caret')
+    console.log(e.target.getAttribute('data-expanded'))
+
+    setStyle({
+      transition: 'transform 200ms ease-out 0s',
+      transform: e.target.getAttribute('data-expanded') === 'expanded' ? 'rotate(0deg)' : 'rotate(90deg)',
+    })
+
+    setExpanded(expanded === 'expanded' ? '' : 'expanded');
+  }
+
   return (
-    <FaCaretRight className='Expand Icon' />
+    <div className='Expand Icon'><FaCaretRight onClick={rotateCaret} style={style} data-expanded={expanded}/></div>
   )
 }
 
